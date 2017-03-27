5389c392384b41a694c8f564e7d84c332a8237e5
syzko <bensingersteven@outlook.com>
2017-03-27 14:55:06 -0400

    Update Battleship.cs

--- Battleship.cs
+++ Battleship.cs
@@ -1,10 +1,11 @@
 /*////////////////////////////////////////////////////////////////
-// Programmers: Steven Bensinger, Dudly Harris, Dennis Loosey   //
-// Course: GSD311				                                //
+// Programmers: Steven Bensinger, Dudly Harris, Dennis Losey    //
+// Course: GSD311                                               //
 // Program Name: Seminar 6 Deliverable 2                        //
-// Created: 03/26/2017                  				        //
-// Last Modified: 03/27/2017			                        //
+// Created: 03/26/2017                                          //
+// Last Modified: 03/27/2017                                    //
 ////////////////////////////////////////////////////////////////*/
+
 
 using System;
 
