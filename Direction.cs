53e457c6b3f29a0e9a2f1a3031015c3ffd5f3671
syzko <bensingersteven@outlook.com>
2017-03-27 14:57:39 -0400

    Update Direction.cs

--- Direction.cs
+++ Direction.cs
@@ -1,4 +1,12 @@
-﻿namespace Week6
+/*////////////////////////////////////////////////////////////////
+// Programmers: Steven Bensinger, Dudly Harris, Dennis Losey    //
+// Course: GSD311                                               //
+// Program Name: Seminar 6 Deliverable 2                        //
+// Created: 03/26/2017                                          //
+// Last Modified: 03/27/2017                                    //
+////////////////////////////////////////////////////////////////*/
+
+namespace Week6
 {
     public enum Direction
     {
