34df5c67b984317d0f612abb5db0a6ad30220d3b
syzko <bensingersteven@outlook.com>
2017-03-27 15:14:02 -0400

    Update Ship.cs

--- Ship.cs
+++ Ship.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*////////////////////////////////////////////////////////////////
+// Programmers: Steven Bensinger, Dudly Harris, Dennis Losey    //
+// Course: GSD311                                               //
+// Program Name: Seminar 6 Deliverable 2                        //
+// Created: 03/26/2017                                          //
+// Last Modified: 03/27/2017                                    //
+////////////////////////////////////////////////////////////////*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
