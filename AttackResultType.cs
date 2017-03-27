554894f0500476ea3a24697ab3e1b09f850eccea
syzko <bensingersteven@outlook.com>
2017-03-27 14:52:59 -0400

    Update AttackResultType.cs

--- AttackResultType.cs
+++ AttackResultType.cs
@@ -1,4 +1,12 @@
-﻿namespace Week6
+/*////////////////////////////////////////////////////////////////
+// Programmers: Steven Bensinger, Dudly Harris, Dennis Loosey   //
+// Course: GSD311				                                //
+// Program Name: Seminar 6 Deliverable 2                        //
+// Created: 03/26/2017                  				        //
+// Last Modified: 03/27/2017			                        //
+////////////////////////////////////////////////////////////////*/﻿
+
+namespace Week6
 {
     public enum AttackResultType
     {
